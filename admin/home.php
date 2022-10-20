5da2e639297049c3748e4147c0c3bff2e8cfa0b8
xnavd <duongxuanvan.official@gmail.com>
2022-10-21 01:25:34 +0700

    update bg-gray

--- admin/home.php
+++ admin/home.php
@@ -1,3 +1,3 @@
-<h1  class="container mx-auto text-4xl text-red-500">
-    Trang chủ
+<h1  class="container mx-auto text-center text-4xl text-red-500">
+    Chào mừng đến với trang quản trị của Bouti
 </h1>
